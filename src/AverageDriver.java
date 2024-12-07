954b0f2fe3915323b7d6f5d9d9612e257975bd90
tchnxcal <tchnxcal@gmail.com>
2024-12-07 18:34:46 -0500

    Comments
    Added comments to AverageDriver.java

--- src/AverageDriver.java
+++ src/AverageDriver.java
@@ -1,6 +1,10 @@
+// Driver class to test the Average class functionality
 public class AverageDriver {
     public static void main(String[] args) {
+        // Create an instance of the Average class which handles input, sorting, and mean calculation
         Average avg = new Average();
+
+        // Output the results (sorted scores and mean) using the toString method
         System.out.println(avg);
     }
 }
